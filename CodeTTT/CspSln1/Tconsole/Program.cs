7ae4ece139fe1e4994fd5b75bec1c9666511318b
kitefano <kitefano@163.com>
2025-08-09 23:45:36 +0800

    modify by lxk@20250809

--- CodeTTT/CspSln1/Tconsole/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tconsole;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
--- CodeTTT/CspSln1/ts2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ts2;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
